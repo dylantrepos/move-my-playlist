45996917e5954e8611606d311f73cfebd6b254c7
Dylan Trepos <dylan.trepos@gmail.com>
2023-12-04 17:47:51 +0100

    feat(chore): add proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['@tanstack/react-query']
-  }
-})
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+
+  return defineConfig({
+    plugins: [react()],
+    optimizeDeps: {
+      exclude: ['@tanstack/react-query']
+    },
+    server: {
+      proxy: {
+        '/deezer-token': {
+          target: 'https://connect.deezer.com/oauth/access_token.php',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/spotify-token': {
+          target: 'https://accounts.spotify.com/api/token',
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  })
+}
