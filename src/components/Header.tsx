f39c750f8dd2c7e4097371c1ddc969df6e1a1b77
Dylan Trepos <dylan.trepos@gmail.com>
2023-12-31 13:58:07 +0100

    feat(header): implement dropdown selection

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,33 @@
 import { Link } from 'react-router-dom';
 import './styles/Header.scss';
+import { useEffect, useRef, useState } from 'react';
+import DeezerIcon from '../assets/images/deezer-sm.png';
+import SpotifyIcon from '../assets/images/spotify-sm.png';
+import Arrow from '../assets/images/arrow.png';
 
 export const Header = () => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setDropdownOpen(false);
+      }
+    };
+
+    if (dropdownOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownOpen]);
 
   return (
     <header className='header__container'>
@@ -11,6 +37,19 @@ export const Header = () => {
       >
         Move My Playlist
       </Link>
+      <div className='header__dropdown' ref={dropdownRef}>
+        <button onClick={toggleDropdown}>Options V</button>
+        {dropdownOpen && (
+          <div className='header__dropdown-menu'>
+            <button onClick={() => console.log('Option 1 clicked')}>
+              <img src={SpotifyIcon}/>
+              <img src={Arrow}/>
+              <img src={DeezerIcon}/>
+            </button>
+            <button onClick={() => console.log('Option 2 clicked')}>Option 2</button>
+          </div>
+        )}
+      </div>
     </header>
   )
 }
