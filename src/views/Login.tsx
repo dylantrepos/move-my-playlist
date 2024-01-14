e98598733446521acd955ec6ef67d459ccf3ab4a
Dylan Trepos <dylan.trepos@gmail.com>
2024-01-14 19:48:52 +0100

    fix(login): test strict mode

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -11,14 +11,14 @@ export default function Login() {
     deezer: false,
     spotify: false,
   })
-  const hasLoaded = useRef(false);
+  // const hasLoaded = useRef(false);
   const [isCookieCheckLoaded, setIsCookieCheckLoaded] = useState(false);
 
   const updateDeezerConnection = () => setCurrentConnection((old) => ({ ...old, deezer: true }));
   const updateSpotifyConnection = () => setCurrentConnection((old) => ({...old, spotify: true}));
 
   useEffect(() => {
-    if(hasLoaded.current) {
+    // if(hasLoaded.current) {
       const checkExistingTokens = async () => {
         const deezerCookieToken = await getCookieDeezerToken();
         const spotifyCookieToken = await getCookieSpotifyToken();
@@ -34,10 +34,10 @@ export default function Login() {
       if (currentConnection.deezer && currentConnection.spotify) {
         setTimeout(() => navigate('/home'), 500);
       }
-    }
+    // }
 
     return () => {
-      hasLoaded.current = true;
+      // hasLoaded.current = true;
     }
   }, [currentConnection, navigate])
 
